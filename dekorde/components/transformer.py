8953a11148768171ab803c2ad0763994e7cf9998
Yuuraa <yoorachoi8937@gmail.com>
2021-10-09 02:55:43 +0900

    Add positional encodings & Change notations && TODO: add mask for decoder

--- dekorde/components/transformer.py
+++ dekorde/components/transformer.py
@@ -1,58 +1,66 @@
 import torch
+import torch.nn as nn
 from dekorde.components.encoder import Encoder
 from dekorde.components.decoder import Decoder
+from dekorde.components.pos_encodings import PositionalEncoding
 
 
-class Transformer(torch.nn.Module):
+# FIXME: mask 들어갈 곳
+class Transformer(nn.Module):
 
-    def __init__(self, embed_size: int, vocab_size: int, hidden_size: int, max_length: int, heads: int, depth: int):
+    def __init__(self, embed_size: int, vocab_size: int, hidden_size: int, max_length: int, heads: int, depth: int, dropout_prob: float = 0.1):
         super().__init__()
         self.embed_size = embed_size
         self.hidden_size = hidden_size
         self.heads = heads
         self.depth = depth
-        # any layers to optimise?
-        # TODO - determine the number of embeddings
-        self.token_embeddings = torch.nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size)
-        self.pos_embeddings = torch.nn.Embedding(num_embeddings=max_length, embedding_dim=embed_size)
-        self.encoder = Encoder(embed_size, hidden_size, heads, depth)  # the encoder stack
-        self.decoder = Decoder(embed_size, hidden_size, heads, depth)  # the decoder stack
 
-    def forward(self, X: torch.Tensor, Y: torch.Tensor, M: torch.Tensor) -> torch.Tensor:
+        # any layers to optimise?
+        self.token_embeddings = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size)
+
+        self.pos_encodings = PositionalEncoding(max_length, embed_size, dropout_prob)
+
+        self.encoder = Encoder(max_length, vocab_size, embed_size, hidden_size, heads, depth, dropout_prob)  # the encoder stack
+        self.decoder = Decoder(max_length, vocab_size, embed_size, hidden_size, heads, depth, dropout_prob)  # the decoder stack
+        self.softmax = nn.SoftMax()
+
+    def forward(self, X: torch.Tensor, Y: torch.Tensor, Mask: torch.Tensor) -> torch.Tensor:
         """
-        :param X: (N, L)
-        :param Y: (N, L)
-        :param M: (N, L, L) - attention mask
-        :return: H_all_y: (N, L, H)
+        :param X: (B, L)
+        :param Y: (B, L)
+        :param Mask: (B, L) - attention mask
+        :return: H_all_y: (B, L, H)
         """
-        # TODO - h
-        X_e = self.token_embeddings(...)
-        X_p = self.pos_embeddings(...)
-        X_ep = X_e + X_p  # positional encoding
-        H_all_x = self.encoder(X_ep)  # (N, L) -> (N, L, H)
-        # TODO
-        Y_e = self.token_embeddings(...)
-        Y_p = self.pos_embeddings(...)
-        Y_ep = Y_e + Y_p  # positional encoding
-        H_all_y = self.decoder(Y_ep, H_all_x, M)  # (N, L, E), (N, L, H), (N, L, L) -> (N, L, H)
-        return H_all_y
+        X_e = self.token_embeddings(X) # (B, L, E)
+        X_ep = self.pos_encodings(X_e) # (B, L, E)
+        H_all_x = self.encoder(X_ep)  # (B, L, E) -> (B, L, E)
+
+        Y_e = self.token_embeddings(Y) # (B, L, E)
+        Y_ep = self.pos_encodings(Y_e) # (B, L, E)
+        H_all_y = self.decoder(Y_ep, H_all_x, Mask)  # (B, L, E), (B, L, E), (B, L) -> (B, L, H)
+        pred = self.predict(H_all_y) # (B, V) 여야 하는데;.. (B, L, V) 가 나와버리는 건가?
+        return pred
 
     def predict(self, H_all_y: torch.Tensor) -> torch.Tensor:
         """
-        :param H_all_y: (N, L, H)
-        :return: S (N, L, |V|)
+        :param H_all_y: (B, L, H)
+        :return: S (B, L, |V|)
         """
-        # S = torch.bmm(H_all_y, self.token_embeddings.weight.T.expand(N, V, H))
         S = torch.einsum("nlh,vh->nlv", H_all_y, self.token_embeddings.weight)  # (N, L, H) * (|V|, E=H) -> (N, L, |V|)
+        S = self.softmax(S)
+        return S
 
-    def training_step(self, X: torch.Tensor, Y: torch.Tensor, M: torch.Tensor) -> torch.Tensor:
+    def training_step(self, X: torch.Tensor, Y: torch.Tensor, Mask: torch.Tensor, Label: torch.Tensor) -> torch.Tensor:
         """
-        :param X: (N, L) - source
-        :param Y: (N, L) - target
-        :param M: (???) - attention mask
-        :return: loss (1,)
+        :param X: (B, L) - source
+        :param Y: (B, L) - target
+        :param Mask: (B, L) - attention mask
+        :return: loss (B, 1)
         """
-        H_all_y = self.forward(X, Y, M)
-        # TODO - compute the loss.
+        pred = self.forward(X, Y, Mask)  # (B, V)
 
-        raise NotImplementedError
+
+
+        return 
+
+

6101b5cafb330c39c6270bd089ca220f800ccf33
Yuuraa <yoorachoi8937@gmail.com>
2021-10-09 02:54:07 +0900

    Change notations

--- dekorde/components/mha.py
+++ dekorde/components/mha.py
@@ -25,15 +25,15 @@ class MultiHeadAttentionLayer(nn.Module):
     # TODO: EP의 뜻이 뭔지...??
     def forward(self, EP_q: torch.Tensor, EP_k: torch.Tensor, EP_v: torch.Tensor, Mask: torch.Tensor = None) -> torch.Tensor:
         """
-        :param EP_q: (N, L, E)
-        :param EP_k: (N, L, E)
-        :param EP_v: (N, L, E)
-        :param M: (???) The mask.
-        :return: H_all (N, L, H)
+        :param EP_q: (B, L, E)
+        :param EP_k: (B, L, E)
+        :param EP_v: (B, L, E)
+        :param M: (B, L) The mask.
+        :return: H_all (B, L, E)
         """
-        Q = self.W_q(EP_q) # (B, L, H * N_head)
-        K = self.W_k(EP_k) # (B, L, H * N_head)
-        V = self.W_v(EP_v) # (B, L, H * N_head)
+        Q = self.W_q(EP_q) # (B, L, E * N_head)
+        K = self.W_k(EP_k) # (B, L, E * N_head)
+        V = self.W_v(EP_v) # (B, L, E * N_head)
         return self.scaled_dot_product_attention(Q, K, V, Mask)
 
     def scaled_dot_product_attention(self,
@@ -42,17 +42,17 @@ class MultiHeadAttentionLayer(nn.Module):
                                      V: torch.Tensor, 
                                      Mask: torch.Tensor) -> torch.Tensor:
         """
-        :param Q: (N, L, H)
-        :param K: (N, L, H)
-        :param V: (N, L, H)
-        :param M: (???)
-        :return: H_all (N, L, H)
+        :param Q: (B, L, E)
+        :param K: (B, L, E)
+        :param V: (B, L, E)
+        :param Mask: (B, L)
+        :return: H_all (B, L, H)
         """
-        attn_scores = torch.matmul(Q, K) # (N, L)
+        attn_scores = torch.matmul(Q, K) # (B, L)
         
         # Masked Multi Head Attention
         if self.masked: 
             assert ( Mask != None ), "Masked Multi head attention require mask as input"
             attn_scores = attn_scores.masked_fill(Mask == 0, -float('inf'))
-        V = torch.dot(attn_scores, V) # (N, L, H)
+        V = torch.dot(attn_scores, V) # (B, L, H)
         return V

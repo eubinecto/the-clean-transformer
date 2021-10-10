1f2465826b5f243420e4e9f030b87b86475e7b7c
John JongYoon Kim <mw18074@bristol.ac.uk>
2021-10-10 11:58:37 +0900

    positional encoding implemented

---
+++ dekorde/components/pos_encoder.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self,
+                 d_model: int,
+                 dropout_rate: int = 0,
+                 max_len: int = 5000):
+        super(PositionalEncoding).__init__()
+
+        # base dropout rate is set as 0. => fully wired.
+        self.dropout = nn.Dropout(p=dropout_rate)
+
+        # ======= calculate positional encoding ======= #
+        # pos_encoding 행렬 세팅
+        pos_enc = torch.zeros(max_len, d_model)     # (max_len, d_model) -> all zeros
+
+        # 0 부터 max_len까지 주루륵
+        position = torch.arange(0, max_len)         # (1, max_len)
+        position = position.unsqueeze(1)            # (1, max_len) -> (max_len, 1)
+
+        # 삼각함수 내의 각도 구하기
+        angle = position / torch.pow(
+            input=torch.tensor(10000.0),
+            exponent=torch.arange(0, d_model, 2) / d_model
+        )
+
+        pos_enc[:, 0::2] = torch.sin(angle)         # 짝수번째 칼럼만 값 업데이트 [0::2]
+        pos_enc[:, 1::2] = torch.cos(angle)         # 홀수번째 칼럼만 값 업데이트  [1::2]
+
+        self.positional_encoding = pos_enc.unsqueeze(0)
+
+    def forward(self, x):
+        return self.dropout(
+            x
+            + Variable(self.positional_encoding[:, :x.size(1)])
+        )
--- dekorde/components/transformer.py
+++ dekorde/components/transformer.py
@@ -1,11 +1,18 @@
 import torch
 from dekorde.components.encoder import Encoder
 from dekorde.components.decoder import Decoder
+from dekorde.components.pos_encoder import PositionalEncoding
 
 
 class Transformer(torch.nn.Module):
+    def __init__(self,
+                 embed_size: int,
+                 vocab_size: int,
+                 hidden_size: int,
+                 max_length: int,
+                 heads: int,
+                 depth: int):
 
-    def __init__(self, embed_size: int, vocab_size: int, hidden_size: int, max_length: int, heads: int, depth: int):
         super().__init__()
         self.embed_size = embed_size
         self.hidden_size = hidden_size
@@ -14,7 +21,7 @@ class Transformer(torch.nn.Module):
         # any layers to optimise?
         # TODO - determine the number of embeddings
         self.token_embeddings = torch.nn.Embedding(num_embeddings=vocab_size, embedding_dim=embed_size)
-        self.pos_embeddings = torch.nn.Embedding(num_embeddings=max_length, embedding_dim=embed_size)
+        self.pos_encodings = PositionalEncoding(d_model=..., max_len=max_length)
         self.encoder = Encoder(embed_size, hidden_size, heads, depth)  # the encoder stack
         self.decoder = Decoder(embed_size, hidden_size, heads, depth)  # the decoder stack
 
@@ -25,16 +32,30 @@ class Transformer(torch.nn.Module):
         :param M: (N, L, L) - attention mask
         :return: H_all_y: (N, L, H)
         """
-        # TODO - h
-        X_e = self.token_embeddings(...)
-        X_p = self.pos_embeddings(...)
-        X_ep = X_e + X_p  # positional encoding
-        H_all_x = self.encoder(X_ep)  # (N, L) -> (N, L, H)
-        # TODO
-        Y_e = self.token_embeddings(...)
-        Y_p = self.pos_embeddings(...)
-        Y_ep = Y_e + Y_p  # positional encoding
-        H_all_y = self.decoder(Y_ep, H_all_x, M)  # (N, L, E), (N, L, H), (N, L, L) -> (N, L, H)
+
+        # ============ ENCODER ============ #
+        # input embedding 구하기
+        X_e = self.token_embeddings(X)
+
+        # positional encoding 구하기
+        # + encoder input 구하기
+        # 이걸하려면 임베딩된 벡터가 있어야 할 것 같아서 X_e를 들고온다.
+        encoder_input = self.pos_encodings(X_e)
+
+        # encoder stack output (hidden vector)
+        H_all_x = self.encoder(encoder_input)  # (N, L) -> (N, L, H)
+
+        # ============ DECODER ============ #
+        # output embedding 구하기 (shifted right)
+        Y_e = self.token_embeddings(Y)
+
+        # positional encoding 구하기
+        # + decoder input
+        decoder_input = self.pos_encodings(Y_e)
+
+        # 잠깐만... M이 필요한가?
+        H_all_y = self.decoder(decoder_input, H_all_x, M)  # (N, L, E), (N, L, H), (N, L, L) -> (N, L, H)
+
         return H_all_y
 
     def predict(self, H_all_y: torch.Tensor) -> torch.Tensor:

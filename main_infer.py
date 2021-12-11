fec218b75568e8fe876d9015cb2cd186661303ef
eubinecto <eubinecto>
2021-12-12 00:54:16 +0900

    inference is now successful

--- main_infer.py
+++ main_infer.py
@@ -6,7 +6,7 @@ from enkorde.fetchers import fetch_config, fetch_tokenizer, fetch_transformer
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", type=str, default="transformer_torch")
+    parser.add_argument("--model", type=str, default="transformer_scratch")
     parser.add_argument("--ver", type=str, default="overfit_small")
     parser.add_argument("--kor", type=str, default="그러나 이것은 또한 책상도 필요로 하지 않는다.")
     args = parser.parse_args()
@@ -17,13 +17,11 @@ def main():
         tokenizer = fetch_tokenizer(run, config['tokenizer'])
         transformer = fetch_transformer(run, config['model'], config['ver'])
         transformer.eval()
-        kors = [config['kor']]
-        X = InferInputsBuilder(tokenizer, config['max_length'])(srcs=kors)
-        src_ids = X[:, 0, 0].squeeze().tolist()  # (1, 2, 2, L) -> (1, L) -> (L) -> list
+        X = InferInputsBuilder(tokenizer, config['max_length'])(srcs=[config['kor']])
+        src_ids = X[0, 0, 0].tolist()  # (1, 2, 2, L) -> (L) -> list
         pred_ids = transformer.predict(X).squeeze().tolist()  # (1, L) -> (L) -> list
-        print([tokenizer.id_to_token(src_id) for src_id in src_ids])
-        print("->")
-        print([tokenizer.id_to_token(pred_id) for pred_id in pred_ids])
+        pred_ids = pred_ids[: pred_ids.index(tokenizer.eos_token_id)]  # noqa
+        print(tokenizer.decode(ids=src_ids), "->", tokenizer.decode(ids=pred_ids))
 
 
 if __name__ == '__main__':

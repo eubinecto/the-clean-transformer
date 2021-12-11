8bb164b95ce4f9b792217434cb9d095cf2bfc782
eubinecto <eubinecto>
2021-12-12 03:09:42 +0900

    torchtext added to the requirements

--- examples/explore_kor2eng_small.py
+++ examples/explore_kor2eng_small.py
@@ -1,9 +1,5 @@
-
 from enkorde.datamodules import Kor2EngSmallDataModule
-import wandb
-
 from enkorde.fetchers import fetch_tokenizer, fetch_config
-from enkorde.paths import WANDB_DIR
 
 
 def main():
@@ -11,25 +7,11 @@ def main():
     ver = "overfit_small"
     config = fetch_config()['train'][model][ver]
     config.update({'num_workers': 2})
-
-    with wandb.init(entity="eubinecto", project="dekorde", dir=WANDB_DIR) as run:
-        tokenizer = fetch_tokenizer(run, config['tokenizer'])
-        datamodule = Kor2EngSmallDataModule(config, tokenizer)
-        datamodule.prepare_data()
-        # --- explore some data --- #
-        for pair in datamodule.kor2eng_train:
-            print(pair)
-        # --- explore the tensors --- #
-        dataloader = datamodule.train_dataloader()
-        for batch in dataloader:
-            X, y = batch
-            src_ids = X[:, 0, 0]
-            tgt_ids = X[:, 1, 0]
-            print([tokenizer.id_to_token(src_id) for src_id in src_ids[0].tolist()])
-            print([tokenizer.id_to_token(tgt_id) for tgt_id in tgt_ids[0].tolist()])
-            # should be right-shifted
-            print([tokenizer.id_to_token(y_id) for y_id in y[0].tolist()])
-            print("-----")
+    datamodule = Kor2EngSmallDataModule(config, None)  # noqa
+    datamodule.prepare_data()
+    # --- explore some data --- #
+    for pair in datamodule.kor2eng_train:
+        print(pair)
 
 
 if __name__ == '__main__':
--- main_infer.py
+++ main_infer.py
@@ -9,7 +9,7 @@ def main():
     parser.add_argument("entity", type=str, help="a wandb entity to download artifacts from")
     parser.add_argument("--model", type=str, default="transformer_scratch")
     parser.add_argument("--ver", type=str, default="overfit_small")
-    parser.add_argument("--kor", type=str, default="그러나 이것은 또한 책상도 필요로 하지 않는다.")
+    parser.add_argument("--kor", type=str, default="결정적인 순간에 그들의 능력을 증가시켜 줄 그 무엇이 매우 중요합니다")
     args = parser.parse_args()
     config = fetch_config()['train'][args.model][args.ver]
     config.update(vars(args))

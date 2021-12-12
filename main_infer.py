3b2110ae4818537a9e255e5f84addbc4fb1803f2
eubinecto <eubinecto>
2021-12-12 15:05:07 +0900

    main_infer.py  - the default version changed

--- main_infer.py
+++ main_infer.py
@@ -7,15 +7,15 @@ def main():
     parser = argparse.ArgumentParser()
     # you must provide this
     parser.add_argument("entity", type=str, help="a wandb entity to download artifacts from")
-    parser.add_argument("--ver", type=str, default="overfit_small")
+    parser.add_argument("--ver", type=str, default="overfit_small_scratch")
     parser.add_argument("--kor", type=str, default="결정적인 순간에 그들의 능력을 증가시켜 줄 그 무엇이 매우 중요합니다")
     args = parser.parse_args()
     config = fetch_config()['train'][args.ver]
     config.update(vars(args))
-    # fetch a pre-trained transformer with a pretrained tokenizer
-    tokenizer = fetch_tokenizer(config['entity'], config['tokenizer'])
+    # fetch a pre-trained transformer & and a pre-trained tokenizer
     transformer = fetch_transformer(config['entity'], config['ver'])
-    transformer.eval()
+    tokenizer = fetch_tokenizer(config['entity'], config['tokenizer'])
+    transformer.eval()  # otherwise, the result will be different on every run
     X = InferInputsBuilder(tokenizer, config['max_length'])(srcs=[config['kor']])
     src_ids = X[0, 0, 0].tolist()  # (1, 2, 2, L) -> (L) -> list
     pred_ids = transformer.predict(X).squeeze().tolist()  # (1, L) -> (L) -> list

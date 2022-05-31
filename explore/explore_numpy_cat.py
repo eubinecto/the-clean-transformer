aeea4efb79e02428543e1c16e731876ebbb17831
Eu-Bin KIM <tlrndk123@gmail.com>
2022-05-31 17:51:25 +0100

    [#15] more explore scripts added

---
+++ explore/explore_numpy_cat.py
@@ -0,0 +1,12 @@
+import numpy
+
+
+def main():
+    x_1 = numpy.zeros((10, 7))
+    x_2 = numpy.zeros((10, 7))
+    x = numpy.concatenate([x_1, x_2], axis=0)
+    print(x.shape)
+
+
+if __name__ == '__main__':
+    main()
---
+++ explore/explore_torch_tensor_to_numpy.py
@@ -0,0 +1,12 @@
+import torch
+
+
+def main():
+    x = torch.rand((10, 3))
+    print(x)
+    # should work?
+    print(x.numpy())
+
+
+if __name__ == '__main__':
+    main()
